f5943b48807cd4377fcddd67c2e42c1ab5fd8420
Alex Kordas <alexjkordas@gmail.com>
2012-12-28 14:38:14 -0600

    add insertion sort

---
+++ sorting_algorithms/insertion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int* vals, int length)
+{
+	int i, j, temp;
+	for(j=1; j<length; j++)
+	{
+		i=j;
+		while(j>0 && vals[i-1]>vals[i])
+		{
+			temp = vals[i];
+			vals[i] = vals[i-1];
+			vals[i-1] = temp;
+			i--;
+		}
+	}
+}
+
+int main()
+{
+	int mynums[10] = {1, 7, 7, 5, 2, 3, 6, 9, 10, 0};
+	insertionSort(mynums, 10);
+	int x;
+	for(x=0; x<10; x++)
+	{
+		cout << mynums[x] << "\t";
+	}
+	cout << "\n";
+}
